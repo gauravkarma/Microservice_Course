9b6ffb2b50b1e4c437c7460f7ea4ca2e963b4905
gaurav <gaurav.karma13@gmail.com>
2025-10-05 16:35:57 +0530

    For API versioning CourseDTOV1 and CourseDTOV2 added

--- Downloads/MapperApplication/src/main/java/com/gk/MapperApplication/DTO/StudentReadDTO.java
+++ Downloads/MapperApplication/src/main/java/com/gk/MapperApplication/DTO/StudentReadDTO.java
@@ -1,4 +1,6 @@
 package com.gk.MapperApplication.DTO;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- Downloads/MapperApplication/src/main/java/com/gk/MapperApplication/Entities/Student.java
+++ Downloads/MapperApplication/src/main/java/com/gk/MapperApplication/Entities/Student.java
@@ -1,9 +1,15 @@
 package com.gk.MapperApplication.Entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,4 +25,9 @@ public class Student {
     private int age;
     private long contact;
     private String address; 
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "departmentId",nullable = false) //Foreign key column
+    @JsonBackReference
+    private Department department;
 }
